511894db65affa2833617b2e4d01a77edbc265fc
Abolfazl Alijafari <134207588+Radisix@users.noreply.github.com>
2023-08-18 22:13:47 +0330

    Facorial Complete , Binary Search Complete , Linear Searcher Complete , Bianry Insertion Sorter Complete , Bubble Sorter Complete

---
+++ Session1/Algorithms.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session1
+{
+    public static class Algorithms
+    {
+        public static int Factorial(int Number)
+        {
+            if (Number == 0)
+                return 1;
+            else
+            {
+                return Number * Factorial(Number - 1);
+            }
+        }
+        public static int BinarySearcher(int[] Array, int Search, int Left, int Right)
+        {
+            if (Right >= Left)
+            {
+                int mid = Left + (Right - Left) / 2;
+                if (Array[mid] == Search)
+                {
+                    return mid;
+                }
+                if (Array[mid] > Search)
+                {
+                    return BinarySearcher(Array, Search, Left, mid - 1);
+                }
+                return BinarySearcher(Array, Search, mid + 1, Right);
+            }
+            return -1;
+        }
+        public static int LinearSearcher(int[] Array, int Search)
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] == Search)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public static int[] Binery_InsertionSorter(int[] Arry)
+        {
+            for (int i = 1; i < Arry.Length; i++)
+            {
+                int x = Arry[i];
+                int index = Array.BinarySearch(Arry, 0, i, x);
+                int j = Math.Abs(index + 1);
+
+                Array.Copy(Arry, j, Arry, j + 1,i - j);
+                Arry[j] = x;
+            }
+            return Arry;
+        }
+        public static int[] BubbleSorter(int[] Array)
+        {
+            int n = Array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (Array[j] > Array[j + 1])
+                    {
+                        int save = Array[j];
+                        Array[j] = Array[j + 1];
+                        Array[j + 1] = save;
+                    }
+                }
+            }
+            return Array;
+        }
+    }
+}
--- Session1/Program.cs
+++ Session1/Program.cs
@@ -1,2 +1,143 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Session1;
+
+while (true)
+{
+
+
+    Console.WriteLine("Request : \n 1-Factorial\n 2-Binary Search\n 3-Fast Searcher\n 4-Linear Searcher\n 5-Bianry Insertion Sorter\n 6-Bubble Sorter\n 7-Clear");
+    int code;
+    while (!int.TryParse(Console.ReadLine(), out code))
+    {
+        Console.WriteLine("Request : \n 1-Factorial\n 2-Binary Search\n 3-Fast Searcher\n 4-Linear Searcher\n 5-Bianry Insertion Sorter\n 6-Bubble Sorter\n 7-Clear");
+    }
+    switch (code)
+    {
+        case 1:
+            Factorial();
+            break;
+        case 2:
+            BinarySearcher();
+            break;
+        case 3:
+            Factorial();
+            break;
+        case 4:
+            LinearSearcher();
+            break;
+        case 5:
+            Binery_InsertionSorter();
+            break;
+        case 6:
+            BubbleSorter();
+            break;
+        case 7:
+            Console.Clear();
+            break;
+        default:
+            Console.WriteLine("Pleaze Enter Correct Code.");
+            break;
+    }
+}
+void Factorial()
+{
+    int Number = GetNumber();
+    Console.WriteLine(Algorithms.Factorial(Number)); ;
+}
+void BinarySearcher()
+{
+    int[] Arry = GetArray();
+    Arry = Algorithms.BubbleSorter(Arry);
+    int Number = GetNumber();
+    int index = Algorithms.BinarySearcher(Arry,Number,0,Arry.Length-1);
+    if(index >=0)
+    {
+        Console.WriteLine("Your Input Founded And Index is: {0}",index);
+    }
+    else
+    {
+        Console.WriteLine("Your Input Not Found.");
+    }
+    
+}
+void LinearSearcher()
+{
+    int[] Arry = GetArray();
+    Arry = Algorithms.BubbleSorter(Arry);
+    int Number = GetNumber();
+    int index = Algorithms.LinearSearcher(Arry, Number);
+    if (index >= 0)
+    {
+        Console.WriteLine("Your Input Founded And Index is: {0}", index);
+    }
+    else
+    {
+        Console.WriteLine("Your Input Not Found.");
+    }
+
+}
+void Binery_InsertionSorter()
+{
+    int[] Arry = GetArray();
+    Arry = Algorithms.Binery_InsertionSorter(Arry);
+    foreach (int Number in Arry)
+    {
+        Console.Write(" " + Number + " ");
+    }
+    Console.WriteLine();
+}
+void BubbleSorter()
+{
+    int[] Arry = GetArray();
+    Arry = Algorithms.BubbleSorter(Arry);
+    foreach (int Number in Arry)
+    {
+        Console.Write(" "+Number+" ");
+    }
+    Console.WriteLine();
+}
+int[] GetArray()
+{
+    bool create = false;
+    int number;
+    List<int> numbers = new List<int>();
+    while (!create)
+    {
+        Console.WriteLine("Enter You Array : \n Enter E For Create Array");
+        string input = Console.ReadLine();
+        if (input == "E")
+        {
+            create = true;
+            continue;
+        }
+        else
+        {
+            if (int.TryParse(input, out number))
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Pleaze Enter Correct Input.");
+                continue;
+            }
+        }
+    }
+    int[] numbersArry = new int[numbers.Count];
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        numbersArry[i] = numbers[i];
+    }
+    return numbersArry;
+}
+int GetNumber()
+{
+    int Number;
+    Console.WriteLine("Input Number :");
+    while (!int.TryParse(Console.ReadLine(), out Number))
+    {
+        Console.WriteLine("Input Number :");
+    }
+    return Number;
+}
+
